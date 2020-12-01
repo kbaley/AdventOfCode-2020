ec05ec600049507a59e769c8cde7c3c196d0ada2
Kyle Baley <kyle@baley.org>
2020-12-01 13:35:01 -0500

    Add skeletons for other days

---
+++ Day23/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AoC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var isSample = args.Length > 0 && args[0] == "-s";
+            var fileName = isSample ? "sample.txt" : "input.txt";
+            var input = File.ReadAllLines(fileName)
+                .Select(s => long.Parse(s));
+
+            // Part 1
+
+            // Part 2
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
