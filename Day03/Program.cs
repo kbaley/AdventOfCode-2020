bdcc33e7adc56bf0f3345788f58b4ab488a7d520
Kyle Baley <kyle@baley.org>
2020-12-03 11:38:44 -0500

    Day 3

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -11,15 +11,30 @@ namespace AoC
         {
             var isSample = args.Length > 0 && args[0] == "-s";
             var fileName = isSample ? "sample.txt" : "input.txt";
-            var input = File.ReadAllLines(fileName)
-                .Select(s => long.Parse(s));
+            var input = File.ReadAllLines(fileName);
 
             // Part 1
+            var trees = CountTrees(input, 3, 1);
+            Console.WriteLine($"Trees: {trees}");
 
             // Part 2
+            trees *= CountTrees(input, 1, 1);
+            trees *= CountTrees(input, 5, 1);
+            trees *= CountTrees(input, 7, 1);
+            trees *= CountTrees(input, 1, 2);
+            Console.WriteLine($"Trees: {trees}");
 
             Console.WriteLine("Done");
-            Console.ReadLine();
+        }
+
+        static long CountTrees(string[] input, int right, int down) {
+            var trees = 0;
+            var x = right;
+            for (var y = down; y < input.Length; y += down) {
+                trees += input[y % input.Length][x % input[0].Length] == '#' ? 1 : 0;
+                x += right;
+            }
+            return trees;
         }
     }
 }
