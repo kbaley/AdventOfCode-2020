2026bdd97b1b4e4f80e03d52a4dc709449e047cc
Kyle Baley <kyle@baley.org>
2020-12-04 11:06:46 -0500

    Minor refactoring

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -8,8 +8,8 @@ namespace AoC
 {
     class Program
     {
-        static Dictionary<string, Func<string, bool>> Rules = new Dictionary<string, Func<string, bool>>();
-        static string[] requiredFields = new[] { "byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:"};
+        static readonly Dictionary<string, Func<string, bool>> Rules = new Dictionary<string, Func<string, bool>>();
+        static readonly string[] requiredFields = new[] { "byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:"};
 
         static void Main(string[] args)
         {
@@ -53,16 +53,16 @@ namespace AoC
             return true;
         }
 
-        static Func<int, int, string, bool> yearRule = (start, end, f) => 
+        static readonly Func<int, int, string, bool> yearRule = (start, end, f) => 
             int.TryParse(f, out int year) && year >= start && year <= end; 
 
-        static Func<string, bool> byr = f => yearRule(1920, 2002, f);
-        static Func<string, bool> iyr = f => yearRule(2010, 2020, f);
-        static Func<string, bool> eyr = f => yearRule(2020, 2030, f);
+        static readonly Func<string, bool> byr = f => yearRule(1920, 2002, f);
+        static readonly Func<string, bool> iyr = f => yearRule(2010, 2020, f);
+        static readonly Func<string, bool> eyr = f => yearRule(2020, 2030, f);
 
-        static Func<string, bool> hgt = f => {
-            var unit = f.Substring(f.Length - 2);
-            if (int.TryParse(f.Substring(0, f.Length - 2), out int measure)) {
+        static readonly Func<string, bool> hgt = f => {
+            var unit = f[^2..];
+            if (int.TryParse(f[0..^2], out int measure)) {
                 if (unit == "cm") {
                     return measure >= 150 && measure <= 193;
                 } else if (unit == "in") {
@@ -72,10 +72,11 @@ namespace AoC
             return false;
         };
 
-        static Func<string, bool> hcl = f => Regex.Match(f, "^[#][0-9a-f]{6}$").Success;
-        static Func<string, bool> ecl = f => new[] {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}.Contains(f);
-        static Func<string, bool> pid = f => Regex.Match(f, "^[0-9]{9}$").Success;
-        static Func<string, bool> cid => f => true;
+        static readonly Func<string, bool> hcl = f => Regex.Match(f, "^[#][0-9a-f]{6}$").Success;
+        static readonly Func<string, bool> ecl = f => new[] {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}.Contains(f);
+        static readonly Func<string, bool> pid = f => Regex.Match(f, "^[0-9]{9}$").Success;
+
+        static readonly Func<string, bool> cid = f => true;
     }
 
 }
