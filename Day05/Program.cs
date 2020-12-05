0bfd8dfc04196ee3a18c329cc6fc006f5139b78d
Kyle Baley <kyle@baley.org>
2020-12-05 14:14:53 -0500

    Day 5

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -11,15 +10,23 @@ namespace AoC
         {
             var isSample = args.Length > 0 && args[0] == "-s";
             var fileName = isSample ? "sample.txt" : "input.txt";
-            var input = File.ReadAllLines(fileName)
-                .Select(s => long.Parse(s));
+            var input = File.ReadAllLines(fileName);
 
             // Part 1
+            var ids = input.Select(i => GetSeatId(i));
+            Console.WriteLine($"Max seat ID: {ids.Max()}");
 
             // Part 2
+            var nums = Enumerable.Range(ids.Min(), ids.Max() - ids.Min());
+            var missingId = nums.First(n => !ids.Contains(n));
+            Console.WriteLine($"My seat ID: {missingId}");
 
             Console.WriteLine("Done");
-            Console.ReadLine();
+        }
+
+        static int GetSeatId(string input) {
+            var binary = input.Replace('B', '1').Replace('F', '0').Replace('R', '1').Replace('L', '0');
+            return Convert.ToInt32(binary, 2);
         }
     }
 }
