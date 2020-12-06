2940555f039d1e8e697cfede2e943c028fec9d26
Kyle Baley <kyle@baley.org>
2020-12-06 18:04:05 -0500

    Day 6

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -11,15 +10,25 @@ namespace AoC
         {
             var isSample = args.Length > 0 && args[0] == "-s";
             var fileName = isSample ? "sample.txt" : "input.txt";
-            var input = File.ReadAllLines(fileName)
-                .Select(s => long.Parse(s));
+            var groups = File.ReadAllText(fileName)
+                .Split("\n\n");
 
             // Part 1
+            var consolidated = groups.Select(g => g.Replace("\n", ""));
+            var sum = consolidated.Sum(g => g.Distinct().Count());
+            Console.WriteLine($"Part one: {sum}");
+
 
             // Part 2
-
+            sum = 0;
+            foreach (var item in groups)
+            {
+                var items = item.Split("\n");
+                var questions = item.Replace("\n", "").Distinct();
+                sum += questions.Count(q => items.All(i => i.Contains(q)));
+            }
+            Console.WriteLine($"Part two: {sum}");
             Console.WriteLine("Done");
-            Console.ReadLine();
         }
     }
 }
