7a27d51e14a9957fa93c00189d1e29ca1b0a04b6
Kyle Baley <kyle@baley.org>
2020-12-08 15:39:53 -0500

    Day 8

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace AoC
 {
@@ -11,15 +10,66 @@ namespace AoC
         {
             var isSample = args.Length > 0 && args[0] == "-s";
             var fileName = isSample ? "sample.txt" : "input.txt";
-            var input = File.ReadAllLines(fileName)
-                .Select(s => long.Parse(s));
+            var input = File.ReadAllLines(fileName);
 
             // Part 1
+            var (_, acc) = RunCode(input);
+            Console.WriteLine($"Part 1: {acc}");
 
             // Part 2
-
+            for (var i = 0; i < input.Length; i++)
+            {
+                var instruction = input[i].Split(' ')[0];
+                if (instruction == "nop" || instruction == "jmp") {
+                    var revisedInput = (string[])input.Clone();
+                    if (instruction.StartsWith("nop")) {
+                        revisedInput[i] = "jmp " + input[i].Split(' ')[1];
+                    } else if (instruction.StartsWith("jmp")) {
+                        revisedInput[i] = "nop +0";
+                    }
+                    bool hasLoop;
+                    (hasLoop, acc) = RunCode(revisedInput);
+                    if (!hasLoop) {
+                        Console.WriteLine($"Part 2: {acc}");
+                        break;
+                    }
+                }
+            }
             Console.WriteLine("Done");
-            Console.ReadLine();
+        }
+
+        private static (string, int) ParseInstruction(string input)
+        {
+            var instruction = input.Split(' ')[0];
+            var val = int.Parse(input.Split(' ')[1]);
+            return (instruction, val);
+        }
+
+        private static (bool, int) RunCode(string[] input) {
+            var acc = 0;
+            var pointer = 0;
+            var visited = new List<int>();
+            while (!visited.Contains(pointer)) {
+                if (pointer >= input.Length) {
+                    return (false, acc);
+                }
+                visited.Add(pointer);
+                var (instruction, value) = ParseInstruction(input[pointer]);
+                switch (instruction) {
+                    case "nop":
+                        pointer++;
+                        break;
+                    case "acc":
+                        acc += value;
+                        pointer++;
+                        break;
+                    case "jmp":
+                        pointer += value;
+                        break;
+                }
+            }
+
+            return (true, acc);
         }
     }
 }
