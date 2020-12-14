6b34e7b10f8164050b046cfa078e1de23a61bf7c
Kyle Baley <kyle@baley.org>
2020-12-14 15:18:36 -0500

    Day 14

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -11,15 +11,49 @@ namespace AoC
         {
             var isSample = args.Length > 0 && args[0] == "-s";
             var fileName = isSample ? "sample.txt" : "input.txt";
-            var input = File.ReadAllLines(fileName)
-                .Select(s => long.Parse(s));
+            var input = File.ReadAllLines(fileName);
+            var start = int.Parse(input[0]);
+            var ids = input[1]
+                .Replace("x", "")
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToList();
 
             // Part 1
+            var min = int.MaxValue;
+            var id = int.MaxValue;
+            foreach (var item in ids)
+            {
+                if (item - (start % item) < min) {
+                    id = item;
+                    min = item - (start % item); 
+                }    
+            }
+            System.Console.WriteLine(id * min);
 
             // Part 2
+            var allIds = input[1].Split(',', StringSplitOptions.RemoveEmptyEntries);
+            var busIndex = input[1].Split(',');
+            var busses = new List<(int bus, int index)>();
+            for (int i = 0; i < busIndex.Length; i++)
+            {
+                if (busIndex[i] != "x") {
+                    busses.Add((int.Parse(busIndex[i]), i));
+                }
+            }
+            long jumpBy = busses.ElementAt(0).bus;
+            long time = jumpBy;
+            for (int i = 1; i < busses.Count(); i++)
+            {
+                while ((time + busses[i].index) % busses[i].bus != 0) {
+                    time += jumpBy;
+                    // System.Console.WriteLine($"{jumpBy},{time},{i}");
+                }
+                jumpBy *= busses[i].bus;
+            }
+            System.Console.WriteLine(time);
 
             Console.WriteLine("Done");
-            Console.ReadLine();
         }
     }
 }
--- Day14/Program.cs
+++ Day14/Program.cs
@@ -11,15 +11,88 @@ namespace AoC
         {
             var isSample = args.Length > 0 && args[0] == "-s";
             var fileName = isSample ? "sample.txt" : "input.txt";
-            var input = File.ReadAllLines(fileName)
-                .Select(s => long.Parse(s));
+            var input = File.ReadAllLines(fileName);
 
             // Part 1
+            var mask = new string('X', 36);
+            var mem = new Dictionary<long, long>();
+            foreach (var item in input)
+            {
+                if (item.StartsWith("mask")) {
+                    mask = item.Replace("mask = ", "");
+                } else {
+                    var location = int.Parse(item[4..].Split(']')[0]);
+                    var value = long.Parse(item.Split(" = ")[1]);
+                    var binary = Convert.ToString(value, 2).PadLeft(36, '0').ToCharArray();
+                    for (int i = 0; i < binary.Length; i++)
+                    {
+                        binary[i] = mask[i] == 'X' ? binary[i] : mask[i];
+                    }
+                    AddToMemory(mem, location, Convert.ToInt64(new string(binary), 2));
+                }
+            }
+            System.Console.WriteLine(mem.Values.Sum());
 
             // Part 2
+            mask = new string('X', 36);
+            mem = new Dictionary<long, long>();
+            foreach (var item in input)
+            {
+                var floats = new List<int>();
+                if (item.StartsWith("mask")) {
+                    mask = item.Replace("mask = ", "");
+                } else {
+                    var location = int.Parse(item[4..].Split(']')[0]);
+                    var value = long.Parse(item.Split(" = ")[1]);
+                    var binary = Convert.ToString(location, 2).PadLeft(36, '0').ToCharArray();
+                    for (int i = 0; i < binary.Length; i++)
+                    {
+                        if (mask[i] == '1') {
+                            binary[i] = '1';
+                        } else if (mask[i] == 'X') {
+                            floats.Add(35 - i);
+                            binary[i] = '0';
+                        }
+                    };
+                    var start = Convert.ToInt64(new string(binary), 2);
+                    AddToMemory(mem, start, value);
+                    foreach (var combo in GetCombinations(floats))
+                    {
+                        AddToMemory(mem, start + combo, value);
+                    }
+                }
+            }
+            System.Console.WriteLine(mem.Values.Sum());
+
 
             Console.WriteLine("Done");
-            Console.ReadLine();
+        }
+
+        private static void AddToMemory(Dictionary<long, long> mem, long key, long value)
+        {
+            if (mem.ContainsKey(key)) {
+                mem[key] = value;
+            } else {
+                mem.Add(key, value);
+            }
+        }
+
+        private static IEnumerable<long> GetCombinations(IEnumerable<int> floats)
+        {
+            var combos = new List<long>();
+            if (!floats.Any()) return combos;
+            var first = floats.First();
+            combos.Add((long)Math.Pow(2, first));
+            if (floats.Count() == 1) { 
+                return combos;
+            }
+            var rest = GetCombinations(floats.Skip(1));
+            combos.AddRange(rest);
+            foreach (var item in rest)
+            {
+                combos.Add(item + (long)Math.Pow(2, first));
+            }
+            return combos;
         }
     }
 }
