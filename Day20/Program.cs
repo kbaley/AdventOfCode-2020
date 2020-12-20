de0b1677d797283f27b57dc68bbbc8a8d9a7e02f
Kyle Baley <kyle@baley.org>
2020-12-20 16:43:35 -0500

    First pass Day 20 Part 1

--- Day20/Program.cs
+++ Day20/Program.cs
@@ -11,15 +11,101 @@ namespace AoC
         {
             var isSample = args.Length > 0 && args[0] == "-s";
             var fileName = isSample ? "sample.txt" : "input.txt";
-            var input = File.ReadAllLines(fileName)
-                .Select(s => long.Parse(s));
+            var input = File.ReadAllLines(fileName);
+            var tiles = new List<Tile>();
+            var i = 0;
+            while (i < input.Length) {
+                var number = int.Parse(input[i].Replace("Tile ", "").Replace(":", ""));
+                i++;
+                var start = i;
+                for (; i < input.Length && !string.IsNullOrWhiteSpace(input[i]); i++);
+                var data = input.Skip(start).Take(i - start);
+                tiles.Add(new Tile(number, data.ToArray()));
+                i++;
+            }
 
             // Part 1
+            var corners = new List<Tile>();
+            for (i = 0; i < tiles.Count; i++)
+            {
+                var tile = tiles[i];
+                var edges = tile.Edges();
+                var matches = new List<int>();
+                for (var j = 0; j < edges.Length; j++)
+                {
+                    matches.AddRange(tiles.Where(t => t.Number != tile.Number && t.Edges().Any(e => e == edges[j])).Select(t => t.Number));
+                }
+                if (matches.Distinct().Count() == 2) {
+                    corners.Add(tile);
+                }
+            }
+            long product = 1;
+            foreach (var tile in corners)
+            {
+                product *= tile.Number;     
+            }
+            System.Console.WriteLine(product);
 
             // Part 2
 
             Console.WriteLine("Done");
-            Console.ReadLine();
+        }
+
+
+    }
+
+    class Tile {
+        public int Number { get; set; }
+        public string[] Data { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string TopEdge {
+            get {
+                return Data[0];
+            }
+        }
+
+        public string BottomEdge {
+            get {
+                return Data[^1];
+            }
+        }
+
+        public string LeftEdge { 
+            get {
+                return new string(Data.Select(i => i[0]).ToArray());
+            }
+        }
+
+        public string RightEdge {
+            get {
+                return new string(Data.Select(i => i[^1]).ToArray());
+            }
+        }
+
+        public string[] Edges() {
+            return new string[] {
+                TopEdge,
+                LeftEdge,
+                BottomEdge,
+                RightEdge,
+                new string(TopEdge.Reverse().ToArray()),
+                new string(LeftEdge.Reverse().ToArray()),
+                new string(BottomEdge.Reverse().ToArray()),
+                new string(RightEdge.Reverse().ToArray())
+            };
+        }
+
+        public Tile(int number, string[] data) {
+            Number = number;
+            Data = data;
+            X = -1;
+            Y = -1;            
+        }
+
+        public override string ToString()
+        {
+            return string.Join('\n', Data);
         }
     }
 }
