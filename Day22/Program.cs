a0ba538130487d43ffb09ab1642722ed31ea8922
Kyle Baley <kyle@baley.org>
2020-12-23 10:38:46 -0500

    Day 22 and 23

--- Day22/Program.cs
+++ Day22/Program.cs
@@ -11,15 +11,80 @@ namespace AoC
         {
             var isSample = args.Length > 0 && args[0] == "-s";
             var fileName = isSample ? "sample.txt" : "input.txt";
-            var input = File.ReadAllLines(fileName)
-                .Select(s => long.Parse(s));
+            var input = File.ReadAllLines(fileName);
+
+            var i = 1;
+            while (!string.IsNullOrWhiteSpace(input[i])) i++;
+            var player1 = new Queue<long>(input.Skip(1).Take(i - 1).Select(long.Parse));
+            var player2 = new Queue<long>(input.Skip(player1.Count + 3).Select(long.Parse));
 
             // Part 1
+            while (player1.Any() && player2.Any()) {
+                var (p1, p2) = (player1.Dequeue(), player2.Dequeue());
+                if (p1 > p2) {
+                    player1.Enqueue(p1);
+                    player1.Enqueue(p2);
+                } else {
+                    player2.Enqueue(p2);
+                    player2.Enqueue(p1);
+                }
+            }
+            var winner = player1.Any() ? player1 : player2;
+            var score = 0L;
+            var multiplier = winner.Count;
+            while (winner.Any()) {
+                score += multiplier-- * winner.Dequeue();
+            }
+            System.Console.WriteLine($"Part 1: {score}");
 
             // Part 2
+            player1 = new Queue<long>(input.Skip(1).Take(i - 1).Select(long.Parse));
+            player2 = new Queue<long>(input.Skip(player1.Count + 3).Select(long.Parse));
+
+            (_, winner) = PlayGame(player1, player2);
+            score = 0L;
+            multiplier = winner.Count;
+            while (winner.Any()) {
+                score += multiplier-- * winner.Dequeue();
+            }
+            System.Console.WriteLine($"Part 2: {score}");
 
             Console.WriteLine("Done");
-            Console.ReadLine();
+        }
+
+        static (bool p1Wins, Queue<long> winningHand) PlayGame(Queue<long> player1, Queue<long> player2) {
+            var playedHands = new HashSet<string>();
+            while (player1.Any() && player2.Any()) {
+                var hand = string.Join(',', player1) + "::" + string.Join(',', player2);
+                if (!playedHands.Add(hand)) return (true, player1);
+
+                (player1, player2) = PlayRound(player1, player2);
+            }
+
+            return player1.Any() ? (true, player1) : (false, player2);
+        }
+
+        private static (Queue<long> player1, Queue<long> player2) PlayRound(Queue<long> player1, Queue<long> player2)
+        {
+            var (p1, p2) = (player1.Dequeue(), player2.Dequeue());
+            bool p1Wins;
+            if (player1.Count >= p1 && player2.Count >= p2) {
+                // Subgame
+                var p1NewHand = new Queue<long>(player1.Take((int)p1));
+                var p2NewHand = new Queue<long>(player2.Take((int)p2));
+                (p1Wins, _) = PlayGame(p1NewHand, p2NewHand);
+            } else {
+                p1Wins = p1 > p2;
+            }
+            if (p1Wins) {
+                player1.Enqueue(p1);
+                player1.Enqueue(p2);
+            } else {
+                player2.Enqueue(p2);
+                player2.Enqueue(p1);
+            }
+
+            return (player1, player2);
         }
     }
 }
--- Day23/Program.cs
+++ Day23/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 
 namespace AoC
@@ -11,15 +10,76 @@ namespace AoC
         {
             var isSample = args.Length > 0 && args[0] == "-s";
             var fileName = isSample ? "sample.txt" : "input.txt";
-            var input = File.ReadAllLines(fileName)
-                .Select(s => long.Parse(s));
+            var input = "219748365";
+
+            var list = new LinkedList<long>(input.Select(i => long.Parse(i.ToString())));
+            var max = list.Max();
+            var nodeList = BuildNodeList(list);
 
             // Part 1
+            for (int i = 0; i < 100; i++)
+            {
+                Move(list, max, nodeList);
+            }
+            while (list.First() != 1) {
+                var first = list.First;
+                list.RemoveFirst();
+                list.AddLast(first);
+            }
+
+            System.Console.WriteLine($"Part 1: {string.Join("", list.Skip(1))}");
 
             // Part 2
+            list = new LinkedList<long>(input.Select(i => long.Parse(i.ToString())));
+            for (var i = max+1; i <= 1_000_000; i++)
+            {
+                list.AddLast(i); 
+            }
+            max = list.Count;
+            nodeList = BuildNodeList(list);
+            for (var i = 0; i < 10_000_000; i++)
+            {
+                Move(list, max, nodeList);
+            }
+            var one = nodeList[1];
+            System.Console.WriteLine($"Part 2: {one.Next.Value * one.Next.Next.Value}");
 
             Console.WriteLine("Done");
-            Console.ReadLine();
+        }
+
+        private static Dictionary<long, LinkedListNode<long>> BuildNodeList(LinkedList<long> list) {
+
+            var max = list.Count;
+            var nodeList = new Dictionary<long, LinkedListNode<long>>();
+            var node = list.First;
+            for (int i = 0; i < max; i++)
+            {
+                nodeList.Add(node.Value, node);
+                node = node.Next;
+            }
+            return nodeList;
+        }
+
+        private static void Move(LinkedList<long> list, long max, Dictionary<long, LinkedListNode<long>> nodeList)
+        {
+            var head = list.First;
+            var one = head.Next;
+            var two = one.Next;
+            var three = two.Next;
+            list.Remove(one);
+            list.Remove(two);
+            list.Remove(three);
+            var target = (head.Value - 2 + max) % max + 1;
+            while (one.Value == target || two.Value == target || three.Value == target)
+            {
+                target = (target - 2 + max) % max + 1;
+            }
+            var targetNode = nodeList[target];
+            list.AddAfter(targetNode, one);
+            list.AddAfter(one, two);
+            list.AddAfter(two, three);
+            list.RemoveFirst();
+            list.AddLast(head);
         }
     }
 }
