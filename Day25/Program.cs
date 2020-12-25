12018f9e1eee6bd5b5b8a00225c0633066148fe9
Kyle Baley <kyle@baley.org>
2020-12-25 13:37:35 -0500

    Day 25

--- Day25/Program.cs
+++ Day25/Program.cs
@@ -12,14 +12,34 @@ namespace AoC
             var isSample = args.Length > 0 && args[0] == "-s";
             var fileName = isSample ? "sample.txt" : "input.txt";
             var input = File.ReadAllLines(fileName)
-                .Select(s => long.Parse(s));
+                .Select(s => long.Parse(s))
+                .ToArray();;
+            var cardKey = input[0];
+            var doorKey = input[1];
+            var subjectNumber = 7;
 
             // Part 1
+            long result = 1;
+            var doorLoop = 1;
+            while (result != doorKey) {
+                result = Transform(result, 7);
+                doorLoop++;
+            }
+            doorLoop--;
+            result = 1;
+            for (int i = 0; i < doorLoop; i++)
+            {
+                result = Transform(result, cardKey);
+            }
+            System.Console.WriteLine($"Part 1: {result}");
 
             // Part 2
 
             Console.WriteLine("Done");
-            Console.ReadLine();
+        }
+
+        static long Transform(long result, long subjectNumber) {
+            return (result *= subjectNumber) % 20201227;
         }
     }
 }
